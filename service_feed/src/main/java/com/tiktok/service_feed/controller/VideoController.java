634cbbd51ebe800f9b14bef4aa9e5cec81f2d439
ToukoYui <2331631097@qq.com>
2023-10-27 23:29:59 +0800

    添加docker-compose文件

--- service_feed/src/main/java/com/tiktok/service_feed/controller/VideoController.java
+++ service_feed/src/main/java/com/tiktok/service_feed/controller/VideoController.java
@@ -1,6 +1,5 @@
 package com.tiktok.service_feed.controller;
 
-import com.tiktok.model.anno.OptionalParamAnno;
 import com.tiktok.model.anno.TokenAuthAnno;
 import com.tiktok.model.vo.TokenAuthSuccess;
 import com.tiktok.model.vo.video.PublishResp;
@@ -9,7 +8,9 @@ import com.tiktok.model.vo.video.VideoVo;
 import com.tiktok.service_feed.service.VideoService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.apache.tomcat.util.http.fileupload.FileUploadBase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,30 +18,32 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
+
 
 @Slf4j
 @RestController
 public class VideoController {
     @Autowired
     private VideoService videoService;
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maximumFileSize;
 
     /**
      * 获取视频流信息（包含作者信息）
      *
-     * @param latestTimeStr    时间戳字符串，处理时要转为DateTime类型
+     * @param latestTimeStr 时间戳字符串，处理时要转为DateTime类型
      * @return
      */
     @GetMapping("/feed")
-    public VideoResp getVideoList(@RequestParam("latest_time") String latestTimeStr,@TokenAuthAnno TokenAuthSuccess tokenAuthSuccess) {
+    public VideoResp getVideoList(@RequestParam("latest_time") String latestTimeStr, @TokenAuthAnno TokenAuthSuccess tokenAuthSuccess) {
         // 如果last_time为空则用当前时间字符串
         Timestamp timestamp = StringUtils.isEmpty(latestTimeStr) ?
                 new Timestamp(System.currentTimeMillis()) : new Timestamp(Long.parseLong(latestTimeStr));
         LocalDateTime localDateTime = timestamp.toLocalDateTime();
         String lastTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
         // 获取视频
-        List<VideoVo> videoVoList = videoService.getVideoList(lastTime,tokenAuthSuccess);
+        List<VideoVo> videoVoList = videoService.getVideoList(lastTime, tokenAuthSuccess);
         // 获取最早发布的视频的发布时间
         LocalDateTime nextTime = videoVoList.get(videoVoList.size() - 1).getCreatedTime();
         Integer nextTimeInt = Math.toIntExact(nextTime.toEpochSecond(ZoneOffset.of("+8")));
@@ -49,6 +52,7 @@ public class VideoController {
 
     /**
      * 视频上传
+     *
      * @param multipartFile
      * @param title
      * @param tokenAuthSuccess
@@ -56,14 +60,22 @@ public class VideoController {
      */
     @PostMapping("/publish/action")
     public PublishResp publishVideo(@RequestParam("data") MultipartFile multipartFile, String title, @TokenAuthAnno TokenAuthSuccess tokenAuthSuccess) {
-        if (!tokenAuthSuccess.getIsSuccess()) {
-            return new PublishResp(403, "token错误，禁止访问");
+        try {
+            if (!tokenAuthSuccess.getIsSuccess()) {
+                return new PublishResp(403, "token错误，禁止访问");
+            }
+            String videoUrl = videoService.uploadVideo(multipartFile, title, tokenAuthSuccess.getUserId());
+            if (StringUtils.isEmpty(videoUrl)) {
+                return new PublishResp(400, "视频发布失败");
+            }
+            log.info("视频上传成功，上传路径为----------------->" + videoUrl);
+        } catch (Exception e) {
+            if (e instanceof FileUploadBase.FileSizeLimitExceededException) {
+                return new PublishResp(0, "发布视频大小超过" + maximumFileSize + "，发布失败");
+            } else {
+                log.error(e.getMessage());
+            }
         }
-        String videoUrl = videoService.uploadVideo(multipartFile, title,tokenAuthSuccess.getUserId());
-        if (StringUtils.isEmpty(videoUrl)) {
-            return new PublishResp(400, "视频发布失败");
-        }
-        log.info("视频上传成功，上传路径为----------------->" + videoUrl);
         return new PublishResp(0, "视频发布成功");
     }
 
@@ -76,8 +88,8 @@ public class VideoController {
      */
     @GetMapping("/publish/list")
     public VideoResp getMyVideoList(@TokenAuthAnno TokenAuthSuccess tokenAuthSuccess) {
-        if (tokenAuthSuccess == null || !tokenAuthSuccess.getIsSuccess()){
-            return new VideoResp("500","请先登录亲~",null,null);
+        if (tokenAuthSuccess == null || !tokenAuthSuccess.getIsSuccess()) {
+            return new VideoResp("500", "请先登录亲~", null, null);
         }
         // 获取当前用户发布的视频,并返回
         List<VideoVo> myVideoList = videoService.getMyVideoList(tokenAuthSuccess);
@@ -87,22 +99,24 @@ public class VideoController {
     /**
      * 获取用户的发布视频数量
      * 内部接口，供user模块调用
+     *
      * @param userId
      * @return
      */
     @GetMapping("/inner/videonum")
-    public Integer getVideoNumByUserId(@RequestParam("userId") Long userId){
+    public Integer getVideoNumByUserId(@RequestParam("userId") Long userId) {
         return videoService.getVideoNumByUserId(userId);
     }
 
     /**
      * 根据视频id列表查询视频信息列表
      * 内部接口，供favorite模块调用
+     *
      * @param videoIdList
      * @return
      */
     @GetMapping("/inner/videoinfolist")
-    public List<VideoVo> getVideoInfoList(@RequestParam("videoIdList") List<Long> videoIdList){
-       return  videoService.getVideoInfoList(videoIdList);
+    public List<VideoVo> getVideoInfoList(@RequestParam("videoIdList") List<Long> videoIdList) {
+        return videoService.getVideoInfoList(videoIdList);
     }
 }
